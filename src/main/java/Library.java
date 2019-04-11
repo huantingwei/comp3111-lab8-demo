c8c3b68a2bd177faafcedf2e4fca027d2f512bd8
hweiac <hweiac@csl4wk20.win2k.cs.ust.hk>
2019-04-11 09:57:18 +0800

    commit for kim

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,7 +3,7 @@
  */
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("this is redo");
+		System.out.println("this is redo for the second time damn!!!");
 	}
     public boolean someLibraryMethod() {
         return true;
